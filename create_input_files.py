8afc1408f37ff86cb71a2b7289e3bf20e3869ab6
Xinsen LU <xinsenlu@gmail.com>
2019-12-16 15:39:27 -0500

    change dataset to flickr8k

--- create_input_files.py
+++ create_input_files.py
@@ -2,10 +2,10 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption_data/',
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='dataset_flickr8k.json',
+                       image_folder='/media/Flickr_Data/Flickr_Data/Images',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption_data/',
+                       output_folder='media/Flickr_Data/Flickr_Data/Images',
                        max_len=50)
