c119e86728ba1decdd1a29a6dac355204921b3db
Xinsen LU <xinsenlu@gmail.com>
2019-12-16 15:52:23 -0500

    change dataset to flickr8k

--- create_input_files.py
+++ create_input_files.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     # Create input files (along with word map)
     create_input_files(dataset='flickr8k',
                        karpathy_json_path='dataset_flickr8k.json',
-                       image_folder='/media/Flickr_Data/Flickr_Data/Images',
+                       image_folder='media/Flickr_Data/Flickr_Data/Images',
                        captions_per_image=5,
                        min_word_freq=5,
                        output_folder='media/Flickr_Data/Flickr_Data/Images',
--- train.py
+++ train.py
@@ -11,8 +11,8 @@ from utils import *
 from nltk.translate.bleu_score import corpus_bleu
 
 # Data parameters
-data_folder = '/media/ssd/caption_data'  # folder with data files saved by create_input_files.py
-data_name = 'coco_5_cap_per_img_5_min_word_freq'  # base name shared by data files
+data_folder = 'media/Flickr_Data/Flickr_Data/Images'  # folder with data files saved by create_input_files.py
+data_name = 'flickr8k_5_cap_per_img_5_min_word_freq'  # base name shared by data files
 
 # Model parameters
 emb_dim = 512  # dimension of word embeddings
