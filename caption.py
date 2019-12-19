deaf5dd070ff51ca6ce2f8f47b1901d84752c7cc
Xinsen LU <xinsenlu@gmail.com>
2019-12-19 13:19:15 -0500

    plot pic

--- caption.py
+++ caption.py
@@ -225,5 +225,5 @@ if __name__ == '__main__':
     alphas = torch.FloatTensor(alphas)
 
     # Visualize caption and attention of best sequence
-    visualize_att(args.img, seq, alphas, rev_word_map, args.smooth)
+    # visualize_att(args.img, seq, alphas, rev_word_map, args.smooth)
     visualize(args.img, seq, alphas, rev_word_map, args.smooth)
